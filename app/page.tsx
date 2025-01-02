d9b147b7467398eb6a35c8046cb6f81d6b8a6bac
Soumyadeep Mondal <soumyadeep589@gmail.com>
2025-01-02 14:44:46 +0530

    feat: add images to assets folder and set the position and blurred effect

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,13 @@ import { gsap } from "gsap";
 import { useEffect } from "react";
 import Image from "next/image";
 import Dog from "../assets/husky.jpg";
+import Elephant from "../assets/elephant.jpg";
+import BoySmile from "../assets/boySmile.jpg";
+import Dolphin from "../assets/dolphin.jpg";
+import GirlSmile from "../assets/girlSmile.jpg";
+import Mountain from "../assets/mountain.jpg";
+import Robot from "../assets/robot.jpg";
+import Clouds from "../assets/clouds.jpg";
 
 export default function Home() {
   useEffect(() => {
@@ -14,22 +21,101 @@ export default function Home() {
       .from(".button", { y: 50, opacity: 0 }, "-=0.6")
       .from(
         ".image",
-        { y: 50, opacity: 0, rotateY: -60, transformOrigin: "center" },
+        {
+          y: 100,
+          opacity: 0,
+          rotateY: 75,
+          transformOrigin: "center",
+          duration: 5,
+        },
         "-=0.8"
       );
   }, []);
   return (
-    <div className="pt-16 overflow-hidden">
+    <div className="overflow-hidden">
       <main>
         <article>
           <div className="min-h-screen bg-white flex flex-col items-center justify-center space-y-6 font-sans">
-            <div className="flex space-x-4 absolute top-0 left-1/2 transform -translate-x-1/2">
+            <div className="flex space-x-4 absolute top-[-10%] left-[58%] transform -translate-x-1/2 z-10">
               <div className="image">
                 <Image
                   src={Dog}
-                  alt="Image 1"
+                  alt="Image Dog"
                   width={200}
-                  height={200}
+                  height={150}
+                  className="rounded-2xl shadow-lg"
+                />
+              </div>
+            </div>
+            <div className="flex space-x-4 absolute top-[-1%] left-[35%] transform -translate-x-1/2 z-10">
+              <div className="image">
+                <Image
+                  src={Elephant}
+                  alt="Image Elephant"
+                  width={290}
+                  height={150}
+                  className="rounded-2xl"
+                />
+              </div>
+            </div>
+            <div className="flex space-x-4 absolute top-[6%] left-[21%] transform -translate-x-1/2 z-5">
+              <div className="image filter blur-sm">
+                <Image
+                  src={BoySmile}
+                  alt="Image Boy Smile"
+                  width={150}
+                  height={80}
+                  className="rounded-2xl shadow-lg"
+                />
+              </div>
+            </div>
+            <div className="flex space-x-4 absolute top-[10%] left-[78%] transform -translate-x-1/2 z-5">
+              <div className="image filter blur-sm">
+                <Image
+                  src={Dolphin}
+                  alt="Image Boy Smile"
+                  width={150}
+                  height={80}
+                  className="rounded-2xl shadow-lg"
+                />
+              </div>
+            </div>
+            <div className="flex space-x-4 absolute top-[40%] left-[78%] z-10">
+              <div className="image">
+                <Image
+                  src={GirlSmile}
+                  alt="Image Girl Smile"
+                  width={350}
+                  className="rounded-2xl shadow-lg"
+                />
+              </div>
+            </div>
+            <div className="flex space-x-4 absolute top-[75%] left-[45%] z-10">
+              <div className="image">
+                <Image
+                  src={Mountain}
+                  alt="Image Mountain"
+                  width={350}
+                  className="rounded-2xl shadow-lg"
+                />
+              </div>
+            </div>
+            <div className="flex space-x-4 absolute top-[55%] left-[10%] z-10">
+              <div className="image">
+                <Image
+                  src={Robot}
+                  alt="Image Robot"
+                  width={300}
+                  className="rounded-2xl shadow-lg"
+                />
+              </div>
+            </div>
+            <div className="flex space-x-4 absolute top-[80%] left-[-5%] z-5">
+              <div className="image filter blur-sm">
+                <Image
+                  src={Clouds}
+                  alt="Image Clouds"
+                  width={300}
                   className="rounded-2xl shadow-lg"
                 />
               </div>
